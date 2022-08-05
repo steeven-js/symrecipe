354bb67e2aa1671f925b6a12387b658caff83fd2
root <root@linux.home>
2022-08-04 20:09:59 -0400

    ✨ Add homepage

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,21 +2,34 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
+use Faker\Generator;
 use App\Entity\Ingredient;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var Generator
+     */
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
 
     public function load(ObjectManager $manager): void
     {
-        $ingredient = new Ingredient();
-        $ingredient->setName('Ingredient #1')
-            ->setPrice(3.0);
 
-        $manager->persist($ingredient);
+        for ($i = 0; $i <= 50; $i++) { 
+            $ingredient = new Ingredient();
+            $ingredient->setName($this->faker->word())
+                ->setPrice(mt_rand(0, 100));
+
+            $manager->persist($ingredient);
+        }
 
         $manager->flush();
         
